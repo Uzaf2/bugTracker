1ca1c6794cab41e7d6bbe7475702c885f1857531
Usman Zafar <Uzman.Zafar1@gmail.com>
2021-02-07 14:52:34 +1100

    First version of completed side bar

--- client/src/pages/ManageUserRoles.js
+++ client/src/pages/ManageUserRoles.js
@@ -1,5 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import '../css/main.css';
+
 import grid from '../icons/grid-outline.svg';
 import homeIcon from '../icons/home-outline.svg';
 import layers from '../icons/layers-outline.svg';
@@ -12,158 +13,97 @@ import personCircleOutline from '../icons/person-circle-outline.svg';
 import reorderFourOutline from '../icons/reorder-four-outline.svg';
 //import users from 'users.svg';
 
-const useStyles = makeStyles((theme) => ({
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: '#F5F8FB',
-    width: '100%',
-    height: '100%',
-  },
-}));
-
 function ManageUserRoles(props) {
 
+  function openNav(e) {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 
-  function handleClick(e) {
-  
-      document.getElementById('navbar').style.width = '100px';
-      document.getElementById('navName').style.fontSize = '0';
-      document.getElementById('grid').style.width = '30px';
-  
-      document.getElementById('navName1').style.fontSize = '0';
-      document.getElementById('grid1').style.width = '30px';
-  
-      document.getElementById('navName2').style.fontSize = '0';
-      document.getElementById('grid2').style.width = '30px';
-  
-      document.getElementById('navName3').style.fontSize = '0';
-      document.getElementById('grid3').style.width = '30px';
-  
-      document.getElementById('navName4').style.fontSize = '0';
-      document.getElementById('grid4').style.width = '30px';
-  
-      document.getElementById('navName5').style.fontSize = '0';
-      document.getElementById('grid5').style.width = '30px';
-
-     /* var one = document.getElementById("navbar");
-      var two = document.getElementById("2");
-
-      var style = window.getComputedStyle(one);
-      var wdt = style.getPropertyValue('width');
-      var totalWidth = window.screen.width;
-      var derived = totalWidth -wdt;
-    
-      two.style.width = derived;
-      console.log("Value", derived);
-      document.getElementById('2').style.width = derived;
-*/
-    document.getElementById('2').style.width = '900px';
-    document.getElementById('2').style.float= 'right';
-    document.getElementById('2').style.textAlign = 'center';
-  }
-
-  function secondHandleClick (e) {
-    document.getElementById('navbar').style.width = '270px';
     document.getElementById('navName').style.fontSize = '.875rem';
-    document.getElementById('grid').style.width = '30px';
-
     document.getElementById('navName1').style.fontSize = '.875rem';
-    document.getElementById('grid1').style.width = '30px';
-
     document.getElementById('navName2').style.fontSize = '.875rem';
-    document.getElementById('grid2').style.width = '30px';
-
     document.getElementById('navName3').style.fontSize = '.875rem';
-    document.getElementById('grid3').style.width = '30px';
-
     document.getElementById('navName4').style.fontSize = '.875rem';
-    document.getElementById('grid4').style.width = '30px';
-
     document.getElementById('navName5').style.fontSize = '.875rem';
+
+    
+    document.getElementById('grid').style.width = '30px';
+    document.getElementById('grid1').style.width = '30px';
+    document.getElementById('grid2').style.width = '30px';
+    document.getElementById('grid3').style.width = '30px';
+    document.getElementById('grid4').style.width = '30px';
+    document.getElementById('grid5').style.width = '30px';
+  }
+  
+  function closeNav(e) {
+    document.getElementById('mySidebar').style.width = '60px';
+    document.getElementById('main').style.marginLeft= '60px';
+
+    document.getElementById('navName').style.fontSize = '0px';
+    document.getElementById('navName1').style.fontSize = '0px';
+    document.getElementById('navName2').style.fontSize = '0px';
+    document.getElementById('navName3').style.fontSize = '0px';
+    document.getElementById('navName4').style.fontSize = '0px';
+    document.getElementById('navName5').style.fontSize = '0px';
+
+  
+    document.getElementById('grid').style.width = '30px';
+    document.getElementById('grid1').style.width = '30px';
+    document.getElementById('grid2').style.width = '30px';
+    document.getElementById('grid3').style.width = '30px';
+    document.getElementById('grid4').style.width = '30px';
     document.getElementById('grid5').style.width = '30px';
 
-    document.getElementById('2').style.width = '600px';
-    document.getElementById('2').style.float= 'right';
-    document.getElementById('2').style.textAlign = 'right';
-   /* var one = document.getElementById("navbar");
-    var two = document.getElementById("2");
-
-    var style = window.getComputedStyle(one);
-    var wdt = style.getPropertyValue('width');
-    var totalWidth = window.screen.width;
-    var derived = totalWidth -wdt;
-    two.style.width = derived;
-
-    var newStr = wdt.slice(0, -2);
-
-    var stotal = ( parseInt(totalWidth) - parseInt(newStr) ).toFixed(2);
-     console.log(totalWidth, " ", newStr)
-     console.log(parseInt(stotal).toFixed(2))
-    document.getElementById('2').style.width = parseInt(stotal).toFixed(2);
-    */
-
   }
-  const styles = useStyles();
+ 
   return (
 
-    <body id="body-pd">
-    <div className="mainBody">
-      <div className="navbar" class="navbar" id="navbar">
-        <a className="section" href="#section">
-          <img class="grid" id="grid" src={grid} alt="image al" />
-          <span class="navName" id="navName">Dashboard Home</span>
+    <body>
+      <div id="mySidebar" class="sidebar">
+       
+        <a href="#"className="section">
+        <img class="grid" id="grid" src={grid} alt="image al" />
+        <span class="navName" id="navName">Dashboard Home</span>
         </a>
-
-        <a className="section" href="#section">
-          <img class="grid" id="grid1" src={homeIcon} alt="image al" />
+       
+        <a href="#"className="section">
+        
+        <img class="grid" id="grid1" src={homeIcon} alt="image al" />
           <span class="navName" id="navName1">Manage Role Assignment</span>
         </a>
-
-        <a className="section" href="#section">
-          <img class="grid" id="grid2" src={peopleCirle} alt="image al" />
+        <a href="#" className="section">
+        <img class="grid" id="grid2"  src={peopleCirle} alt="image al" />
           <span class="navName" id="navName2">Manage Project Users</span>
         </a>
-
-        <a className="section" href="#section">
-          <img class="grid" id="grid3" src={layers} alt="image al" />
+        <a href="#" className="section">
+        <img class="grid" id="grid3"  src={layers} alt="image al" />
           <span class="navName" id="navName3">My Projects</span>
         </a>
-
-
-        <a className="section" href="#section">
-          <img class="grid" id="grid4" src={layers} alt="image al" />
+        <a href="#" className="section">
+        <img class="grid" id="grid4"  src={layers} alt="image al" />
           <span class="navName" id="navName4">My Tickets</span>
         </a>
-
-
-        <a className="section" href="#section">
-
+        <a href="#" className="section">
           <img class="grid" id="grid5" src={personAddOutline} alt="image al" />
           <span class="navName" id="navName5">User Profile</span>
         </a>
-
-        <a className="section" href="#section"  onClick={handleClick}>
-          Click me
+        <a className="section" href="#section"  onClick={openNav}>
+          Open Sidebar
           </a>
 
-          <a className="section" href="#section" onClick={secondHandleClick}>
-          UnClick me
+          <a className="section" href="#section" onClick={closeNav}>
+          Close Sidebar
           </a>
       </div>
 
-
-    <div className="body" id="2">
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Atque optio odio officiis nostrum nesciunt quam libero. 
-            Cumque impedit veritatis, quibusdam nulla accusantium illo. 
-            In velit laboriosam obcaecati quaerat eaque beatae.</h1>
-       
+      
+    
+    <div id="main" class="main">
+      <h2>Collapsed Sidebar</h2>
+      <p>Click on the hamburger menu/bar icon to open the sidebar, and push this content to the right.</p>
     </div>
-     
-    </div>
-   
+    
     </body>
-
   );
 }
 
