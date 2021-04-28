e368c1fca42b7ff3a49a8f5b3c61a48694e30c76
Usman Zafar <usmanzafar@192-168-1-105.tpgi.com.au>
2021-04-29 01:46:51 +1000

    Made some changes to fix some issues caused due to TextField issues

--- client/src/pages/CreateProject.js
+++ client/src/pages/CreateProject.js
@@ -1,13 +1,72 @@
 import React, { useState } from 'react';
 //import '../css/createProject.css';
-//import '../css/main.css';
 import gql from 'graphql-tag';
 import SideAndNavbar from '../components/SideAndNavbar';
 import { useMutation } from '@apollo/react-hooks';
 import { useForm } from '../util/hooks';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+    },
+    container: {
+        maxHeight: 440,
+    },
+    label: {
+        fontSize: 12,
+        marginLeft: '30%',
+        fontFamily: 'sans-serif',
+        fontStyle:'italic',
+        fontWeight:'600'
+    },
+    input:{
+        width: '40%',
+        padding: '12px 20px',
+        margin: '8px 0',
+        display: 'inline-block',
+        borderWidth: '1px',
+        borderColor: '#ddd',
+        borderRadius: '4px', 
+        boxSizing: 'border-box',
+        marginLeft: '30%',
+        marginRight: '30%'
+    },
+    submit:{
+        width: '40%',
+        backgroundColor: '#262B40',
+        color: 'white',
+        padding: '14px 20px',
+        margin: '8px 0',
+        display: 'inline-block',
+        borderWidth: '4px',
+        borderRadius: '4px', 
+        marginLeft: '30%',
+        marginRight: '30%',
+        '&:hover': {
+            background: " #F5A623",
+         },
+    },
+ description: {
+    width: '530px',
+    height: '200px',
+    marginLeft: '30%',
+    marginRight: '30%'
+  },
+  title: {
+      marginTop: '10%',
+      paddingTop: '100px',
+      marginLeft: "43%",
+      fontSize: 18,
+      fontWeight: '800',
+      fontFamily: 'sans-serif'
+  }
+});
 
 function CreateProject(props) {
 
+    const classes = useStyles();
     const [errors, setErrors]= useState({});
     const {onChange, onSubmit, values}= useForm(createProject, {
         name:'',
@@ -35,19 +94,21 @@ function CreateProject(props) {
 
         <SideAndNavbar/>
           <form onSubmit={onSubmit} class="inputForm">
-            <label for="name">Project Name</label>
+            <label for="name"
+            className={classes.label}>Project Name</label>
             <input 
             type="text" 
             id="name" 
             name="name" 
+            className={classes.input}
             placeholder="Project name.." 
             autoComplete="name"
             error={errors.name ? true:false}
             value={values.name}
             onChange={onChange}/>
-            <label for="description">Project Description</label>
+            <label for="description" className={classes.label}>Project Description</label>
             <textarea 
-            className="description" 
+            className={classes.description} 
             type="text" 
             id="description" 
             name="description" 
@@ -55,7 +116,7 @@ function CreateProject(props) {
             error={errors.description  ? true:false}
             value={values.description}
             onChange={onChange}></textarea>
-            <input type="submit" value="Create Project" />
+            <input class="submitBtn"  className={classes.submit} type="submit" value="Create Project" />
           </form>
           <div className="ui error message">
                 <ul className="list">
--- client/src/pages/CreateTicket.js
+++ client/src/pages/CreateTicket.js
@@ -1,14 +1,75 @@
 import React, { useState } from 'react';
-//import '../css/createProject.css';
-import '../css/main.css';
+//import '../css/createTicket.css';
+//import '../css/main.css';
 import gql from 'graphql-tag';
 import SideAndNavbar from '../components/SideAndNavbar';
 import { useMutation } from '@apollo/react-hooks';
 import { useForm } from '../util/hooks';
 import { useQuery } from '@apollo/client';
+import { ClassNames } from '@emotion/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+    },
+    container: {
+        maxHeight: 440,
+    },
+    label: {
+        fontSize: 12,
+        marginLeft: '30%',
+        fontFamily: 'sans-serif',
+        fontStyle:'italic',
+        fontWeight:'600'
+    },
+    input:{
+        width: '40%',
+        padding: '12px 20px',
+        margin: '8px 0',
+        display: 'inline-block',
+        borderWidth: '1px',
+        borderColor: '#ddd',
+        borderRadius: '4px', 
+        boxSizing: 'border-box',
+        marginLeft: '30%',
+        marginRight: '30%'
+    },
+    submit:{
+        width: '40%',
+        backgroundColor: '#262B40',
+        color: 'white',
+        padding: '14px 20px',
+        margin: '8px 0',
+        display: 'inline-block',
+        borderWidth: '4px',
+        borderRadius: '4px', 
+        marginLeft: '30%',
+        marginRight: '30%',
+        '&:hover': {
+            background: " #F5A623",
+         },
+    },
+ description: {
+    width: '530px',
+    height: '200px',
+    marginLeft: '30%',
+    marginRight: '30%'
+  },
+  title: {
+      marginTop: '10%',
+      paddingTop: '100px',
+      marginLeft: "43%",
+      fontSize: 18,
+      fontWeight: '800',
+      fontFamily: 'sans-serif'
+  }
+});
 
 function CreateTicket(props) {
+
+    const classes = useStyles();
 
     const [errors, setErrors]= useState({});
     const {onChange, onSubmit, values}= useForm(createProject, {
@@ -100,47 +161,52 @@ function CreateTicket(props) {
 
         <SideAndNavbar/>
           <form onSubmit={onSubmit} class="inputForm">
-            <label for="title">Ticket Title</label>
+            <label className={classes.label}
+            for="title">Ticket Title</label>
             <input 
             type="text" 
             id="title" 
             name="title" 
+            className={classes.input}
             placeholder="Ticket Title.." 
             autoComplete="title"
             error={errors.title ? true:false}
             value={values.title}
             onChange={onChange}/>
-            <label for="description">Ticket Description</label>
+            <label className={classes.label} for="description">Ticket Description</label>
             <textarea 
-            className="description" 
+            className={classes.description} 
             type="text" 
+            className={classes.input}
             id="description" 
             name="description" 
             placeholder="Project desc.." required
             error={errors.description}
             value={values.description}
             onChange={onChange}/>
-            <label for="">Assigned Project</label>
-            <select id="assignedProjectInput" name="assignedProjectInput"
+            <label className={classes.label} for="">Assigned Project</label>
+            <select className={classes.input} id="assignedProjectInput" name="assignedProjectInput"
             value={values.assignedProjectInput} onChange={onChange}>
             </select>
-            <label for="assignedDeveloperInput">Assigned Developer</label>
-            <select id="assignedDeveloperInput" name="assignedDeveloperInput"
+            <label className={classes.label} for="assignedDeveloperInput">Assigned Developer</label>
+            <select className={classes.input} id="assignedDeveloperInput" name="assignedDeveloperInput"
             id="assignedDeveloperInput" name="assignedDeveloperInput"
             value={values.assignedDeveloperInput} onChange={onChange}>
             </select>
-            <label for="priority">Priority</label>
+            <label className={classes.label} for="priority">Priority</label>
             <select id="priority"
             value={values.priority}
             name="priority"
+            className={classes.input}
             onChange={onChange}>
             <option value="Low">Low</option>
             <option value="Medium">Medium</option>
             <option value="High">High</option>
             </select>
-            <label for="type">Ticket Type</label>
+            <label className={classes.label} for="type">Ticket Type</label>
             <select id="type" name="type"
             value={values.type}
+            className={classes.input}
             id="type"
             name="type"
             onChange={onChange}>
@@ -149,15 +215,17 @@ function CreateTicket(props) {
             <option value="Other Comments">Other Comments</option>
             <option value="Training/Document Requests">Training/Document Requests</option>
             </select>
-            <label for="ticketStatus">Ticket Status</label>
-            <select name="status" value={values.status}
+            <label className={classes.label} for="ticketStatus">Ticket Status</label>
+            <select name="status"
+            className={classes.input}
+            value={values.status}
             id="status" name="status"
             onChange={onChange}>
             <option value="completed">Completed</option>
             <option value="open">Open</option>
             <option value="inprogress">In Progress</option>
             </select>
-            <input type="submit" value="Create Ticket"/>
+            <input className={classes.submit} type="submit" value="Create Ticket"/>
           </form>
           <div className="ui error message">
                 <ul className="list">
